f91f7c268a6751d5586868e3671f9bcbe220d323
khouloudadhem1984 <70451350+khouloudadhem1984@users.noreply.github.com>
2020-08-30 20:14:33 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('<h1>My Node App</h1>');
+    res.send('<h1>My Node Adham</h1>');
 });
 
 app.listen(5000,()=>{
-    console.log('App is listening on port 5000');
+    console.log('App is listening on port 6000');
 })
